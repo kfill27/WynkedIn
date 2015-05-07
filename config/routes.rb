d6f9310ee624807763fa1b0bf7f76e64c5f660dd
Kyra Fillmore <kfillmore27@gmail.com>
2015-05-07 06:02:25 -0400

    Bootstrap styling and Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :profiles
+  resources :users, :profiles
 
   get 'sessions/new'
-  get 'signup'  => 'users#new'
-  get 'login'   => 'sessions#new'
-  post 'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
+  get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
   get 'profiles/new' => 'profiles#new'
   get 'profiles' => 'profiles#show'
-
-  root 'users#index'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  
+  root 'profiles#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
